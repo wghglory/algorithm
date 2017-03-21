f37164850a92f7161b15e0705fc14b438d6a273d
Guanghui Wang <wghglory89@gmail.com>
2017-03-21 16:09:51 -0700

    binary tree DFS and BFS

--- DataStructure/Tree/TreeTraverse.cs
+++ DataStructure/Tree/TreeTraverse.cs
@@ -1,85 +1,173 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-class Program
+namespace BinaryTreeTraversal
 {
-
-	static void Main(string[] args)
+	class MainClass
 	{
-		BinaryTreeNode<int> n1 = new BinaryTreeNode<int> { Value = 60 };
-		BinaryTreeNode<int> n2 = new BinaryTreeNode<int> { Value = 30 };
-		BinaryTreeNode<int> n3 = new BinaryTreeNode<int> { Value = 30 };
-		BinaryTreeNode<int> n4 = new BinaryTreeNode<int> { Value = 20 };
-		BinaryTreeNode<int> n5 = new BinaryTreeNode<int> { Value = 50 };
-		BinaryTreeNode<int> n6 = new BinaryTreeNode<int> { Value = 50 };
-		BinaryTreeNode<int> n7 = new BinaryTreeNode<int> { Value = 20 };
-		BinaryTreeNode<int> n8 = new BinaryTreeNode<int> { Value = 15 };
-		BinaryTreeNode<int> n9 = new BinaryTreeNode<int> { Value = 15 };
-
-		BinaryTree<int> tree = new BinaryTree<int>();
-		tree.Root = n1;
-		tree.Root.Left = n2;
-		tree.Root.Right = n3;
-		tree.Root.Left.Left = n4;
-		tree.Root.Left.Right = n5;
-		tree.Root.Left.Right.Left = n8;
-		tree.Root.Right.Left = n6;
-		tree.Root.Right.Left.Right = n9;
-		tree.Root.Right.Right = n7;
-
-		var list = Traverse(n1);
-
-		foreach (var l in list)
+		public static void Main()
 		{
-			Console.Write(l + " ");
+			TreeNode<string> root = defineDataNode();
+			DFS<string> dfs_traversal = new DFS<string>();
+
+			Console.WriteLine("\n================DFS Inorder===================");
+			dfs_traversal.InOrder(root);
+
+			Console.WriteLine("\n================DFS preorder===================");
+			dfs_traversal.PreOrder(root);
+
+			Console.WriteLine("\n================DFS postorder===================");
+			dfs_traversal.PostOrder(root);
+
+			Console.WriteLine("\n============BFS result=======================");
+
+			BFS<string> bfs_traversal = new BFS<string>();
+			bfs_traversal.Traversal(root);
 		}
 
-
-		Console.ReadKey();
-	}
-
-	public static List<int> Traverse(BinaryTreeNode<int> root)
-	{
-		Queue<BinaryTreeNode<int>> queue = new Queue<BinaryTreeNode<int>>();
-		List<int> result = new List<int>();
-
-		queue.Enqueue(root);
-
-		while (queue.Count != 0)
+		private static TreeNode<string> defineDataNode()
 		{
-			BinaryTreeNode<int> node = queue.Dequeue();
-			result.Add(node.Value);
+
+			//          F
+			//        /   \
+			//       B     G
+			//      / \     \
+			//     A   D     I
+			//        / \    /\
+			//       C   E  H  J
 
 
-			if (node.Left != null)
-			{
-				queue.Enqueue(node.Left);
-			}
+			//TreeNode<string> node = new TreeNode<string>();
+			//node.Data = "F";
+			//node.Right = new TreeNode<string>();
+			//node.Left = new TreeNode<string>();
+			//node.Left.Data = "B";
+			//node.Right.Data = "G";
 
-			if (node.Right != null)
-			{
-				queue.Enqueue(node.Right);
-			}
+			//node.Right.Right = new TreeNode<string>();
+			//node.Right.Right.Right = new TreeNode<string>();
+			//node.Left.Left = new TreeNode<string>();
+			//node.Left.Right = new TreeNode<string>();
+			//node.Left.Right.Left = new TreeNode<string>();
+			//node.Left.Right.Right = new TreeNode<string>();
+
+			//node.Left.Left.Data = "A";
+			//node.Left.Right.Data = "D";
+			//node.Left.Right.Left.Data = "C";
+			//node.Left.Right.Right.Data = "E";
+
+			//node.Right.Right.Data = "I";
+			//node.Right.Right.Left = new TreeNode<string>();
+			//node.Right.Right.Left.Data = "H";
+			//node.Right.Right.Right.Data = "J";
+
+			TreeNode<string> node =
+				new TreeNode<string>("F",
+									 new TreeNode<string>("B",
+														  new TreeNode<string>("A"),
+														  new TreeNode<string>("D",
+																				  new TreeNode<string>("C"),
+																				  new TreeNode<string>("E")
+																			  )
+														 ),
+									 new TreeNode<string>("G",
+														  null,
+														  new TreeNode<string>("I",
+																				  new TreeNode<string>("H"),
+																				  new TreeNode<string>("J")
+																			  )
+														 )
+									);
+
+			return node;
 		}
 
-		return result;
 	}
 
 }
 
-public class BinaryTreeNode<T>
+public class TreeNode<T>
 {
-	public BinaryTreeNode<T> Right { get; set; }
-	public BinaryTreeNode<T> Left { get; set; }
-	public T Value { get; set; }
-}
+	public T Data;
+	public TreeNode<T> Left;
+	public TreeNode<T> Right;
 
-public class BinaryTree<T>
-{
-	public BinaryTree()
+	public TreeNode(T data, TreeNode<T> left, TreeNode<T> right)
 	{
-		Root = new BinaryTreeNode<T>();
+		this.Data = data;
+		this.Left = left;
+		this.Right = right;
 	}
 
-	public BinaryTreeNode<T> Root { get; set; }
+	public TreeNode(T data)
+	{
+		this.Data = data;
+		this.Left = null;
+		this.Right = null;
+	}
+
+	public TreeNode() { }
+}
+
+public class DFS<T>  //Depth first search, recursion
+{
+
+	public void PreOrder(TreeNode<T> node)
+	{
+		if (node == null) return;
+		Visit(node);
+		PreOrder(node.Left);
+		PreOrder(node.Right);
+	}
+
+	public void InOrder(TreeNode<T> node)
+	{
+		if (node == null) return;
+		InOrder(node.Left);
+		Visit(node);
+		InOrder(node.Right);
+	}
+
+	public void PostOrder(TreeNode<T> node)
+	{
+		if (node == null) return;
+		PostOrder(node.Left);
+		PostOrder(node.Right);
+		Visit(node);
+	}
+
+	private void Visit(TreeNode<T> node)
+	{
+		Console.Write($"{node.Data} ");
+	}
+}
+
+public class BFS<T>  //breath first search, queue while
+{
+	Queue<TreeNode<T>> queue = new Queue<TreeNode<T>>();
+
+	public void Traversal(TreeNode<T> root)
+	{
+		if (root != null)
+		{
+			queue.Enqueue(root);
+
+			while (queue.Count != 0)
+			{
+				TreeNode<T> node = queue.Dequeue();
+
+				Console.Write($"{node.Data} ");
+
+				if (node.Left != null)
+				{
+					queue.Enqueue(node.Left);
+				}
+
+				if (node.Right != null)
+				{
+					queue.Enqueue(node.Right);
+				}
+			}
+		}
+	}
 }
