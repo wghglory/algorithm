1d37d432c5cbede8cd61792583bc0c71f0fe9ab0
Guanghui Wang <wghglory89@gmail.com>
2017-03-24 17:49:30 -0700

    binary search usage

--- DataStructure/BinarySearch.cs
+++ DataStructure/BinarySearch.cs
@@ -73,3 +73,108 @@ class Program
 		return -1;   // doesn't have target
 	}
 }
+
+
+//binary search usage:
+using System;
+using System.Collections.Generic;
+
+class Program
+{
+	/*1. Find first/last instance's index of a number in sorted array where duplicates are allowed.
+	e.g. find first/last 44
+	find key occurrence: lastIndex - firstIndex + 1;
+	*/
+	static void Main(string[] args)
+	{
+		int[] arr = { 1, 2, 3, 4, 5, 8, 10, 44, 44, 66, 100 };   // 11 numbers
+
+		Console.WriteLine($"first duplicate index: {FindFirstDuplicateIndex(arr, 44)}");
+		Console.WriteLine($"last duplicate index: {FindLastDuplicateIndex(arr, 44)}");
+
+		int[] nums = { 1, 2, 3, 4, 7, 6, 5 };
+		Console.WriteLine($"first decreasing index: {FindDecreasingIndex(nums)}");
+		Console.ReadKey();
+	}
+
+	private static int FindFirstDuplicateIndex(int[] arr, int key)
+	{
+		int leftIndex = 0;
+		int rightIndex = arr.Length - 1;
+
+		while (leftIndex <= rightIndex)
+		{
+			int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+			if (arr[mid] == key && arr[mid - 1] != arr[mid])  //only change here
+			{
+				return mid;
+			}
+			else if (arr[mid] < key)
+			{
+				leftIndex = mid + 1;
+			}
+			else
+			{
+				rightIndex = mid - 1;
+			}
+		}
+
+		return -1;
+	}
+
+	private static int FindLastDuplicateIndex(int[] arr, int key)
+	{
+		int leftIndex = 0;
+		int rightIndex = arr.Length - 1;
+
+		while (leftIndex <= rightIndex)
+		{
+			int mid = leftIndex + (rightIndex - leftIndex) / 2;
+
+			if (arr[mid] == key && arr[mid - 1] == arr[mid])  //only change here
+			{
+				return mid;
+			}
+			else if (arr[mid] < key)
+			{
+				leftIndex = mid + 1;
+			}
+			else
+			{
+				rightIndex = mid - 1;
+			}
+		}
+
+		return -1;
+	}
+
+	//Find a point where arrays starts decreasing, array is first increasing and then decreasing.
+	private static int FindDecreasingIndex(int[] arr)
+	{
+		int left = 0;
+		int right = arr.Length - 1;
+
+		while (left <= right)
+		{
+
+			int mid = left + (right - left) / 2;
+
+			if (arr[mid - 1] < arr[mid] && arr[mid] > arr[mid + 1]) // mid is the biggest number, mid+1 is the first decreasing number
+			{
+				return mid + 1;
+			}
+			else if (arr[mid - 1] < arr[mid] && arr[mid] < arr[mid + 1])  // increasing
+			{
+				left = mid + 1;
+			}
+			else if (arr[mid - 1] > arr[mid] && arr[mid] > arr[mid + 1])  // decreasing
+			{
+				right = mid - 1;
+			}
+		}
+		return -1;
+
+	}
+
+}
