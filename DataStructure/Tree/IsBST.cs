60c0c651457ca73b99f5b7dc8052ae90736421eb
Guanghui Wang <wghglory89@gmail.com>
2017-04-04 14:37:25 -0700

    Update IsBST.cs

--- DataStructure/Tree/IsBST.cs
+++ DataStructure/Tree/IsBST.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+
 /**
 * Date 04/11/2015
 * @author tusroy
@@ -24,136 +26,170 @@ using System.Collections.Generic;
 public class IsBST
 {
 
-	public bool isBST(Node root)
-	{
-		return isBST1(root, int.MinValue, int.MaxValue);
-	}
+    public bool isBST(Node root)
+    {
+        return isBST1(root, int.MinValue, int.MaxValue);
+    }
 
-	//every node: left < parent < right,
-	// min < left < parent
-	// parent < right < max
-	private bool isBST1(Node node, int min, int max)
-	{
-		if (node == null)
-		{
-			return true;
-		}
-		if (node.Data < min || node.Data > max)
-		{
-			return false;
-		}
-		return isBST1(node.Left, min, node.Data) && isBST1(node.Right, node.Data, max);
-	}
-
-
-	public bool isBST2(Node root)
-	{
-		if (root == null) return true;
-
-		Stack<Node> s = new Stack<Node>();
-
-		while (root != null)
-		{
-			s.Push(root);
-			root = root.Left;
-		}
-
-		int prev = int.MinValue;
-		while (s.Count > 0)
-		{
-			Node current = s.Pop();
-
-			Console.WriteLine($"prevData: {prev}, currentData: {current.Data}");
-
-			if (prev > current.Data)
-			{
-				return false;
-			}
-			prev = current.Data;
-
-			Node rightOfCurrent = current.Right;
-
-			while (rightOfCurrent != null)
-			{
-				s.Push(rightOfCurrent);
-				rightOfCurrent = rightOfCurrent.Left;
-			}
-
-		}
-
-		return true;
-	}
+    //every node: left < parent < right,
+    // min < left < parent
+    // parent < right < max
+    private bool isBST1(Node node, int min, int max)
+    {
+        if (node == null)
+        {
+            return true;
+        }
+        if (node.Data < min || node.Data > max)
+        {
+            return false;
+        }
+        return isBST1(node.Left, min, node.Data) && isBST1(node.Right, node.Data, max);
+    }
 
 
-	public static void Main(string[] args)
-	{
-		Node root = DefineBST();
+    public bool isBST2(Node root)
+    {
+        if (root == null) return true;
 
-		IsBST isBST = new IsBST();
+        Stack<Node> s = new Stack<Node>();
 
-		Console.WriteLine(isBST.isBST(root));
-		Console.WriteLine(isBST.isBST2(root));
-	}
+        while (root != null)
+        {
+            s.Push(root);
+            root = root.Left;
+        }
 
-	private static Node DefineBST()
-	{
+        int prev = int.MinValue;
+        while (s.Count > 0)
+        {
+            Node current = s.Pop();
 
-		//          5
-		//        /   \
-		//       2     10
-		//      / \    / \
-		//     1   3   7  12
-		//             /\
-		//            6  7
+            Console.WriteLine($"prevData: {prev}, currentData: {current.Data}");
+
+            if (prev > current.Data)
+            {
+                return false;
+            }
+            prev = current.Data;
+
+            Node rightOfCurrent = current.Right;
+
+            while (rightOfCurrent != null)
+            {
+                s.Push(rightOfCurrent);
+                rightOfCurrent = rightOfCurrent.Left;
+            }
+
+        }
+
+        return true;
+    }
+
+    private static bool result = true;
+    private static int prev = int.MinValue;
+    public static bool IsBst3(Node root)
+    {
+        if (root == null) return false;
+
+        IsBst3(root.Left);
+
+        Console.WriteLine($"prevData: {prev}, currentData: {root.Data}");
+        if (root.Data < prev) result = false;
+        prev = root.Data;
+
+        IsBst3(root.Right);
+
+        return result;
+    }
+
+    public static void PrintCurrentAndParent(Node root, int pVal)
+    {
+        if (root == null) return;
+
+        PrintCurrentAndParent(root.Left, root.Data);
+
+        Console.WriteLine($"parent: {pVal}, currentData: {root.Data}");
+
+        PrintCurrentAndParent(root.Right, root.Data);
+    }
 
 
-		Node node = new Node();
-		node.Data = 5;
-		node.Right = new Node();
-		node.Left = new Node();
+    public static void Main(string[] args)
+    {
+        Node root = DefineBST();
 
-		node.Left.Data = 2;
-		node.Right.Data = 10;
+        IsBST isBST = new IsBST();
 
-		node.Left.Left = new Node();
-		node.Left.Right = new Node();
-		node.Left.Left.Data = 1;
-		node.Left.Right.Data = 3;
+        Console.WriteLine(isBST.isBST(root));
+        Console.WriteLine(isBST.isBST2(root));
+        Console.WriteLine(IsBst3(root));
 
-		node.Right.Left = new Node();
-		node.Right.Right = new Node();
-		node.Right.Left.Data = 7;
-		node.Right.Right.Data = 12;
+        PrintCurrentAndParent(root, int.MinValue);
 
-		node.Right.Left.Left = new Node();
-		node.Right.Left.Right = new Node();
-		node.Right.Left.Left.Data = 6;
-		node.Right.Left.Right.Data = 7;  //7, 8
+        Console.ReadKey();
+    }
 
-		return node;
-	}
+
+    private static Node DefineBST()
+    {
+
+        //          5
+        //        /   \
+        //       2     10
+        //      / \    / \
+        //     1   3   7  12
+        //             /\
+        //            6  7
+
+
+        Node node = new Node();
+        node.Data = 5;
+        node.Right = new Node();
+        node.Left = new Node();
+
+        node.Left.Data = 2;
+        node.Right.Data = 10;
+
+        node.Left.Left = new Node();
+        node.Left.Right = new Node();
+        node.Left.Left.Data = 1;
+        node.Left.Right.Data = 3;
+
+        node.Right.Left = new Node();
+        node.Right.Right = new Node();
+        node.Right.Left.Data = 7;
+        node.Right.Right.Data = 12;
+
+        node.Right.Left.Left = new Node();
+        node.Right.Left.Right = new Node();
+        node.Right.Left.Left.Data = 6;
+        node.Right.Left.Right.Data = 5;  //7, 8
+
+        return node;
+    }
 }
 
 
 public class Node
 {
-	public int Data;
-	public Node Left;
-	public Node Right;
+    public int Data;
+    public Node Left;
+    public Node Right;
 
-	public Node(int Data, Node Left, Node Right)
-	{
-		this.Data = Data;
-		this.Left = Left;
-		this.Right = Right;
-	}
+    public Node(int Data, Node Left, Node Right)
+    {
+        this.Data = Data;
+        this.Left = Left;
+        this.Right = Right;
+    }
 
-	public Node(int Data)
-	{
-		this.Data = Data;
-		this.Left = null;
-		this.Right = null;
-	}
+    public Node(int Data)
+    {
+        this.Data = Data;
+        this.Left = null;
+        this.Right = null;
+    }
 
-	public Node() { }
+    public Node() { }
 }
